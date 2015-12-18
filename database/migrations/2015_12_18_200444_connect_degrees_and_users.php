b5bdb0693812f08ab90a396b313bb78d56e1ad41
Dan November <november@g.harvard.edu>
2015-12-18 15:17:58 -0500

    changes

---
+++ database/migrations/2015_12_18_200444_connect_degrees_and_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ConnectDegreesAndUsers extends Migration
+{
+    /**
+    * Run the migrations.
+    *
+    * @return void
+    */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('degree_id')->unsigned();
+            $table->foreign('degree_id')->references('id')->on('degrees');
+        });
+    }
+
+    /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+    public function down()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->dropForeign('degrees_user_id_foreign');
+            $table->dropColumn('degree_id');
+        });
+    }
+}
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,11 +11,12 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        $degree_id = 1;
         $user = \App\User::firstOrCreate(['email' => 'jill@harvard.edu']);
         $user->name = 'Jill';
         $user->email = 'jill@harvard.edu';
         $user->password = \Hash::make('helloworld');
+        $user->degree_id = $degree_id;
         $user->save();
 
         $user = \App\User::firstOrCreate(['email' => 'jamal@harvard.edu']);
